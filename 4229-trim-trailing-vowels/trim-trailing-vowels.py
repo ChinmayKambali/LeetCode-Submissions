717140038b8ec4824ed51421133a7bd93eb222e6
Chinmay Kambali <chinmaykambali2004@gmail.com>
2026-07-18 12:16:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (11.84%) - LeetSync

---
+++ 4229-trim-trailing-vowels/trim-trailing-vowels.py
@@ -0,0 +1,7 @@
+class Solution:
+    def trimTrailingVowels(self, s: str) -> str:
+        vowels = "AEIOUaeiou"
+        i = len(s) - 1
+        while i >= 0 and s[i] in vowels:
+            i -= 1
+        return s[:i+1]
