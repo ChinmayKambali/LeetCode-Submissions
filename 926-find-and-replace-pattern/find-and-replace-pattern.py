9c21eccdc17e5b663708746949c9e445253e03b1
Chinmay Kambali <chinmaykambali2004@gmail.com>
2026-09-03 08:25:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (35.19%) - LeetSync

--- 926-find-and-replace-pattern/find-and-replace-pattern.py
+++ 926-find-and-replace-pattern/find-and-replace-pattern.py
@@ -6,17 +6,22 @@ class Solution:
             # bijection
             # function to check if the word and pattern satisfy the bijection
             def find(word,pattern):
+
                 map_wordtopattern={}
                 map_patterntoword={}
+
                 for i in range(len(word)):
+                    
                     word_char=word[i]
                     pattern_char=pattern[i]
+                    
                     if word_char in map_wordtopattern:
                         if map_wordtopattern[word_char]!=pattern_char:
                             return False
                     else:
                         if pattern_char in map_patterntoword:
                             return False
+
                     map_wordtopattern[word_char]=pattern_char
                     map_patterntoword[pattern_char]=word_char
                 return True
